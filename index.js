1f1ddc37aae44e30b077dafad866327959116267
rhwette <rhwette@sbcglobal.net>
2022-04-03 10:18:19 -0500

    edit index.html and index.js

--- index.js
+++ index.js
@@ -1,67 +1,187 @@
 
 //-----------------------------------------------
 // associate variables with classes
-//    introButtonPencil = button that activates form
+//    introButtonPencilElement = button that activates form "Edit Profile"
 //    nameVariable = starting name on form
 //    aboutMeVariable = starting occupation on form
-//    popupFormButton = the 'SAVE' button on form
-//    popupFormName = the name entered on form
+//    popupButtonSave = the 'SAVE' button on 'editProfile' form
+//    popupButtonCreate = the 'Create' button on 'newPlace' Form
+//    popupButtonCloseEditProfile = 'big X' on 'editProfile' form
+//    popupButtonCloseNewPlace = 'big X' on 'newPlace' form
+//    popupName = the name entered on form
 //    popupAboutMe = the occupation enterd on form
-//-----------------------------------------------
-const introButtonPencilElement = document.querySelector('.intro__button-pencil');
-const nameVariable = document.querySelector(".intro__name");
-const aboutmeVariable = document.querySelector(".intro__occupation");
-const popupButtonVariable = document.querySelector(".popup__button");
-const popupName=document.querySelector('input[name="name"]');
-const popupAboutMe=document.querySelector('input[name="aboutme"]');
+//    introButtonPlusElement = button that activates form "New Place"
 //-----------------------------------------------
 
+const introButtonPencilElement = document.querySelector('.intro__button-pencil');
+console.log('introButtonPencilElement',introButtonPencilElement);
+const nameVariable = document.querySelector(".intro__name");
+const aboutmeVariable = document.querySelector(".intro__occupation");
+const popupButtonSave= document.querySelector("#popupButtonSave");
+const popupButtonCloseEditProfile = document.querySelector('#popupButtonCloseEditProfile');
+const popupButtonCreate= document.querySelector("popupButtonCreate");
+const popupButtonCloseNewPlace = document.querySelector('#popupButtonCloseNewPlace');
+const popupName=document.querySelector('input[name="name"]');
+const popupAboutMe=document.querySelector('input[name="aboutme"]');
+const introButtonPlusElement = document.querySelector('.intro__button-plus');
+
+//  ARRAY of OBJECTS containing image links
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+]; 
+
+//***********************************************
 //-----------------------------------------------
-//  define function to open the Modal
+//  CONTROLS FOR 'EDIT PROFILE POPUP'
+//-----------------------------------------------
+//***********************************************
+
+//-----------------------------------------------
+//  LISTEN for clicks on introButtonPencil and on popupButtonSAVE
+//-----------------------------------------------
+introButtonPencilElement.addEventListener('click', openModal1);
+popupButtonSave.addEventListener('click', saveButton);
+
+//-----------------------------------------------
+//  FUNCTION 'openModal1'
 //-----------------------------------------------  
-function openModal() {
+function openModal1(evtEditProfile) {
+console.log( 'evtEditProfile:', evtEditProfile);
   popupName.value=nameVariable.textContent;
   popupAboutMe.value=aboutmeVariable.textContent;
-  const containerElement = document.querySelector('.popup-container');
+  const containerElement = document.querySelector('#person-popup-container');
+  console.log("popup container:" , containerElement);
   containerElement.classList.add('popup-container_visible');
 }
 //-----------------------------------------------
-
+//  FUNCTION 'saveButton'
 //-----------------------------------------------
-//  define function that controls form input and SAVE event
-//-----------------------------------------------
-function  saveButton (evt) {
- evt.preventDefault();
+function  saveButton (evtSave) {
+ evtSave.preventDefault();
  nameVariable.textContent = popupName.value;
  aboutmeVariable.textContent = popupAboutMe.value;
- closeModal();
+ closeModal1();
 }
-//-----------------------------------------------
 
 //-----------------------------------------------
-//  listen for clicks on Pencil and on SAVE
-introButtonPencilElement.addEventListener('click', openModal);
-popupButtonVariable.addEventListener('click', saveButton);
+//  LISTEN - click on big X 'editProfile' form
 //-----------------------------------------------
+popupButtonCloseEditProfile.addEventListener('click', closeModal1);
 
 
-//------------------------------------------------
-// define variable for closing Modal
-const popupButtonClose = document.querySelector('.popup__button-close');
 //-----------------------------------------------
-
+//  FUNCTION 'closeModal1'
 //-----------------------------------------------
-//  define function to close the Modal
-function closeModal() {
-  const containerElement = document.querySelector('.popup-container');
+function closeModal1() {
+  const containerElement = document.querySelector('#person-popup-container');
     containerElement.classList.remove('popup-container_visible');
 }
+
+
+//***********************************************
 //-----------------------------------------------
+//  CONTROLS FOR 'NEW PLACE POPUP'
+//-----------------------------------------------
+//***********************************************
 
 //-----------------------------------------------
-//  listen for click on big X
-popupButtonClose.addEventListener('click', closeModal);
+//  LISTEN for clicks on introButtonPlus and on popupButtonCreate
+//-----------------------------------------------
+introButtonPlusElement.addEventListener('click', openModal2);
+popupButtonCreate.addEventListener('click', createButton);
+
+//-----------------------------------------------
+//  FUNCTION 'openModal2'
+//-----------------------------------------------  
+function openModal2(evtNewPlace) {
+  console.log( 'evtNewPlace:', evtNewPlace);
+  // popupName.value=nameVariable.textContent;
+  // popupAboutMe.value=aboutmeVariable.textContent;
+    const containerElement = document.querySelector('#picture-popup-container');
+    console.log("popup container:" , containerElement);
+    containerElement.classList.add('popup-container_visible');
+  }
+
+//-----------------------------------------------
+//  FUNCTION 'createButton'
+//-----------------------------------------------
+function  createButton (evtCreate) {
+  evtCreate.preventDefault();
+  // ???????????????????????delete or replace?????????????????????????????????
+  // nameVariable.textContent = popupName.value;
+  // aboutmeVariable.textContent = popupAboutMe.value;
+  closeModal2();
+ }
+
+ //-----------------------------------------------
+//  LISTEN - click on big X 'newPlace' form
+//-----------------------------------------------
+popupButtonCloseNewPlace.addEventListener('click', closeModal2);
+
+//-----------------------------------------------
+//  FUNCTION 'closeModal2'
+//-----------------------------------------------
+function closeModal2() {
+  const containerElement = document.querySelector('#picture-popup-container');
+    containerElement.classList.remove('popup-container_visible');
+}
+
+
+
+
+
+
+
 //------------------------------------------------
+//  Below code will handle the 'heart' status
+//------------------------------------------------
+
+// const cardGridInfo = document.querySelectorAll('.card-grid__icon');
+// console.log('cardGridInfo:', cardGridInfo);
+// for(let i=0; i<cardGridInfo.length; i++){
+//   console.log('cardGridInfo at index i:',cardGridInfo[i])
+//   cardGridInfo[i].addEventListener('click',changeHeartColor);
+// }
+
+// cardGridInfo.forEach((anyElement) => {
+//   console.log('anyElement:', anyElement);
+// })
+
+// console.log('cardGridInfo after listen:', cardGridInfo);
+
+
+// function changeHeartColor(anynameEvent) {
+//   console.log('event:',anynameEvent.target.id)
+//   console.log('test:',(document.getElementById(anynameEvent.target.id).src))
+//   if(document.getElementById(anynameEvent.target.id).src==="http://127.0.0.1:5500/images/heart.svg") {
+//     document.getElementById(anynameEvent.target.id).src="./images/Union.svg";
+//   } else {
+//     document.getElementById(anynameEvent.target.id).src="./images/heart.svg";
+//   }
+// }
 
 
 
