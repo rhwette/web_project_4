6bf501d2c7e92955fcd9de9371955b81d8ce0ae5
rhwette <rhwette@sbcglobal.net>
2022-07-23 19:30:11 -0500

    FEEDBACK CARDS.JS

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,6 @@
-import { openPopup, closePopup } from "./Utils.js";
+// //  FEEDBACK CARDS.JS (line1) - files which contain class declarations should not import anything
+//    also, not that the import below wasnt being used anyway
+// import { openPopup, closePopup } from "./Utils.js";
 const containerElementImage = document.querySelector("#image-popup-container");
 // these two are not used
 // const imageZoom = document.getElementById("image-zoom");
@@ -10,6 +12,18 @@ class Card {
     this._link = data.link;
     this._cardSelector = cardSelector;
     this._containerElementImage = containerElementImage;
+    // FEEDBACK CARDS.JS (line13) - good....passing the handler with the arguments (of the contstructor)
+    //  is the right way....that way the Card is only responsible for card rendering
+
+    // FEEDBACK CARDS.JS (line 25) -  add this._handleCan in the constructor
+    this._handleCan = this._handleCan.bind(this);
+
+    // FEEDBACK CARDS.JS (line 46) - better to bind methods in the constructor,
+    // FEEDBACK - instead of down below where the method is called
+    // FEEDBACK - also note, right side has no 'this._'
+    this._handleZoom = handleZoom.bind(this);
+    // this._handleZoom = handleZoom;
+
     this._handleZoom = handleZoom;
   }
 
@@ -22,9 +36,16 @@ class Card {
   }
 
   _handleCan() {
-    this.parentElement.remove();
+    // FEEDBACK CARDS.JS (line 25) - we want to remove 'this._element.remove()'
+    // FEEDBACK -  not the parentElement
+    // FEEDBACK -  so include 'this._handleCan = this._handleCan.bind(this);'
+    // FEEDBACK -  in the class constructor
+
+    // this.parentElement.remove();
+    this._element.remove();
   }
 
+  //  FEEDBACK CARDS.JS (line 28) - good - each callback is a separate method
   _handleHeart(event) {
     event.target.classList.toggle("card-grid__icon_active");
   }
@@ -41,9 +62,12 @@ class Card {
       .addEventListener("click", this._handleCan);
 
     // add EventListener for zoomPic
+    // FEEDBACK CARDS.JS (line 46) - better to bind methods in the constructor,
+    // FEEDBACK    not in the spot where the method is used
+    // .addEventListener("click", this._handleZoom.bind(this));
     this._element
       .querySelector("img")
-      .addEventListener("click", this._handleZoom.bind(this));
+      .addEventListener("click", this._handleZoom);
   }
 
   createCard() {
